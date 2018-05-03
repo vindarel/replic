b12c8a687dc3f9c9a308a8eabeee926b8e71340e
vindarel <ehvince@mailz.org>
2018-05-03 16:30:17 +0200

    add quit in base

--- src/base.lisp
+++ src/base.lisp
@@ -12,13 +12,21 @@
                 :*verbose*)
   (:shadowing-import-from :replic
                           :set)
+  (:shadow :quit)
   (:export :help
            :set
            :reload
            :*verbose*
            :highlight
-           :unhighlight))
+           :unhighlight
+           :quit))
+
+(in-package replic.base)
 
 (replic.completion:add-completion "set" #'replic.completion:variables)
 
 (replic.completion:add-completion "help" #'replic::help-completion)
+
+(defun quit ()
+  "Quit the application."
+  (uiop:quit))
